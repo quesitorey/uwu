2c0870edc47d68f654b4d0eb45bad6ea87807cb6
quesitorey <galleta_chipss@hotmail.com>
2023-06-22 10:20:37 +0000

    beta-version

--- src/pages/Letters.jsx
+++ src/pages/Letters.jsx
@@ -4,10 +4,10 @@ const Letters = () => {
   return(
     <div className="letter-body">
       <div className="letter-box">
-        <Link className="letters" to="/letters/1">Letter 1</Link>
+        <Link className="letters" to="/letters/1">1</Link>
       </div>
       <div className="letter-box">
-        <Link className="letters" to="/letters/2">Letter 2</Link>
+        <Link className="letters" to="/letters/2">2</Link>
       </div>
       
     </div>
